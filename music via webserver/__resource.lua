53f816d4242fa1521f2825f69bec8a12c7ae2086
FAXES <31368070+FAXES@users.noreply.github.com>
2018-02-23 10:32:21 +1100

    1.1 Release

---
+++ music via webserver/__resource.lua
@@ -0,0 +1,12 @@
+-- Script Made by FAXES
+files {
+    'index.html',
+    'keks.css',
+	'style.css',
+    'bankgothic.ttf',
+    'loadscreen.jpg',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
