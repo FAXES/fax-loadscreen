44f906754dfd0aaa1c144ca6706bc8d84642a759
FAXES <31368070+FAXES@users.noreply.github.com>
2018-02-23 10:24:59 +1100

    1.1 Release

---
+++ music in resource/__resource.lua
@@ -0,0 +1,17 @@
+-- Script Made by FAXES
+files {
+    'index.html',
+    'keks.css',
+	'style.css',
+    'bankgothic.ttf',
+    'loadscreen.jpg',
+	'music/1.ogg',
+	'music/2.ogg',
+	'music/3.ogg',
+	'music/4.ogg',
+	'music/5.ogg',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
